8dfea417a580eead2e5aa2bacc237f630f5959c0
Ren <renfxu08@gmail.com>
2025-07-23 13:02:22 -0700

    blah

--- main/main.ino
+++ main/main.ino
@@ -1,9 +1,25 @@
+int LED = 11;
+int button = 7;
+int pot = A5;
+int potval = 0;
+int butVal = 0;
+int brightness = 0;
+
 void setup() {
   // put your setup code here, to run once:
-
+  pinMode(LED, OUTPUT);
+  pinMode(button, INPUT_PULLUP);
+  pinMode(pot, OUTPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  potval = analogRead(pot);
+  butVal = digitalRead(button);
+  brightness = potval/4;
 
+  if (butVal == LOW) {
+    analogWrite(LED, 0);
+  } else {
+    analogWrite(LED, brightness);
+  }
 }
